452156b470edb501aced1483e0df6df96d84a318
Adhirohah232 <banerjeeamlan100@gmail.com>
2024-07-09 21:34:45 +0530

    post words UI added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -16,6 +16,12 @@ const Quiz = () => {
   const [showWordPairs, setShowWordPairs] = useState(false);
   const [showSearchResult, setShowSearchResult] = useState(false);
   const [showDateWordPairs, setShowDateWordPairs] = useState(false);
+  const [passkey, setPasskey] = useState('');
+  const [passkeyError, setPasskeyError] = useState('');
+  const [canAddWords, setCanAddWords] = useState(false);
+  const [showPasskeyInput, setShowPasskeyInput] = useState(false);
+  const [wordInputs, setWordInputs] = useState([{ word: '', synonyms: '' }]);
+  const [bulkWords, setBulkWords] = useState('');
 
   const fetchWordPairs = async () => {
     if (showWordPairs) {
@@ -35,10 +41,6 @@ const Quiz = () => {
   };
 
   const searchWordPair = async () => {
-    if (showSearchResult) {
-      setShowSearchResult(false);
-      return;
-    }
     setLoading(true);
     setSearchResult(null);
     setSearchError('');
@@ -87,6 +89,92 @@ const Quiz = () => {
     }
   };
 
+  const handleAddWordsButtonClick = () => {
+    setShowPasskeyInput(true);
+  };
+
+  const handlePasskeySubmit = () => {
+    if (passkey === '232527') {
+      setCanAddWords(true);
+      setPasskeyError('');
+    } else {
+      setCanAddWords(false);
+      setPasskeyError('Incorrect passkey');
+    }
+  };
+
+  const handleWordChange = (index, event) => {
+    const values = [...wordInputs];
+    values[index][event.target.name] = event.target.value;
+    setWordInputs(values);
+  };
+
+  const handleAddWordInput = () => {
+    setWordInputs([...wordInputs, { word: '', synonyms: '' }]);
+  };
+
+  const handleSubmitWords = async () => {
+    if (!selectedDate) {
+      alert('Please select a date');
+      return;
+    }
+
+    const formattedDate = format(selectedDate, 'd/M/yyyy');
+    const words = {};
+
+    wordInputs.forEach(({ word, synonyms }) => {
+      words[word] = synonyms.split(',').map((synonym) => synonym.trim());
+    });
+
+    const requestBody = {
+      date: formattedDate,
+      words: words,
+    };
+
+    try {
+      await axios.post('https://wordly-backend.onrender.com/words', requestBody, { withCredentials: false });
+      alert('Words added successfully');
+    } catch (error) {
+      console.error('Error adding words:', error);
+      alert('Failed to add words');
+    }
+  };
+
+  const handleBulkWordsChange = (event) => {
+    setBulkWords(event.target.value);
+  };
+
+  const handleSubmitBulkWords = async () => {
+    if (!selectedDate) {
+      alert('Please select a date');
+      return;
+    }
+
+    const formattedDate = format(selectedDate, 'd/M/yyyy');
+    const words = {};
+    const lines = bulkWords.split('\n');
+
+    lines.forEach(line => {
+      const parts = line.split(':');
+      const word = parts[0].trim();
+      const synonyms = parts[1].split(',').map(synonym => synonym.trim());
+      words[word] = synonyms;
+    });
+
+    const requestBody = {
+      date: formattedDate,
+      words: words,
+    };
+
+    try {
+      await axios.post('https://wordly-backend.onrender.com/words', requestBody, { withCredentials: false });
+      alert('Words added successfully');
+    } catch (error) {
+      console.error('Error adding words:', error);
+      alert('Failed to add words');
+    }
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <div className="max-w-3xl mx-auto bg-white p-6 rounded shadow">
@@ -122,7 +210,7 @@ const Quiz = () => {
             onClick={searchWordPair}
             disabled={loading}
           >
-            {loading ? 'Searching...' : showSearchResult ? 'Hide Search Result' : 'Search'}
+            {loading ? 'Searching...' : 'Search'}
           </button>
           {showSearchResult && searchResult && (
             <div className="mt-4">
@@ -164,6 +252,104 @@ const Quiz = () => {
           {dateError && (
             <div className="mt-4 text-red-500">
               {dateError}
+            </div>
+          )}
+        </div>
+
+        <div className="mt-6">
+          <p className="text-xl mb-4">Want to add words?</p>
+          {!showPasskeyInput && (
+            <button
+              className="bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500"
+              onClick={handleAddWordsButtonClick}
+            >
+              Add Words
+            </button>
+          )}
+          {showPasskeyInput && (
+            <div>
+              <input
+                type="password"
+                className="border p-2 mr-2"
+                placeholder="Enter passkey"
+                value={passkey}
+                onChange={(e) => setPasskey(e.target.value)}
+              />
+              <button
+                className="bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500"
+                onClick={handlePasskeySubmit}
+              >
+                Submit Passkey
+              </button>
+              {passkeyError && (
+                <div className="mt-4 text-red-500">
+                  {passkeyError}
+                </div>
+              )}
+              {canAddWords && (
+                <>
+                  <div className="mt-4">
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={(date) => setSelectedDate(date)}
+                      dateFormat="dd/MM/yyyy"
+                      className="border p-2 mr-2"
+                      placeholderText="Select a date"
+                    />
+                  </div>
+                  {wordInputs.map((input, index) => (
+                    <div key={index} className="mt-2">
+                      <input
+                        type="text"
+                        name="word"
+                        className="border p-2 mr-2"
+                        placeholder="Enter word"
+                        value={input.word}
+                        onChange={(event) => handleWordChange(index, event)}
+                      />
+                      <input
+                        type="text"
+                        name="synonyms"
+                        className="border p-2 mr-2"
+                        placeholder="Enter synonyms (comma separated)"
+                        value={input.synonyms}
+                        onChange={(event) => handleWordChange(index, event)}
+                      />
+                      {index === wordInputs.length - 1 && (
+                        <>
+                          <button
+                            className="bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500 mr-2"
+                            onClick={handleSubmitWords}
+                          >
+                            Add Words
+                          </button>
+                          <button
+                            className="bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500"
+                            onClick={handleAddWordInput}
+                          >
+                            +
+                          </button>
+                        </>
+                      )}
+                    </div>
+                  ))}
+                  <div className="mt-4">
+                    <textarea
+                      className="border p-2 w-full"
+                      rows="6"
+                      placeholder="Enter words and synonyms in bulk. Format: Word: synonym1, synonym2, ..."
+                      value={bulkWords}
+                      onChange={handleBulkWordsChange}
+                    />
+                    <button
+                      className="bg-green-400 text-white py-2 px-4 rounded hover:bg-green-500 mt-2"
+                      onClick={handleSubmitBulkWords}
+                    >
+                      Add Bulk Words
+                    </button>
+                  </div>
+                </>
+              )}
             </div>
           )}
         </div>
